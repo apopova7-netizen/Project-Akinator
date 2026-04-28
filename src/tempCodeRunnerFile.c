1a81c71dea0b4625e228a95fb174920e9e7c258f
zagumennikovavika <zagumennikovavs@gmail.com>
2026-04-28 22:47:01 +0700

    Delete src/tempCodeRunnerFile.c

--- src/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
- // if (lineNumber != 2) {
-            //     printf("Error in line %d: root must be on line 2 (right after statistics)\n", lineNumber);
-            //     fclose(file);
-            //     return 0;
-            // }
