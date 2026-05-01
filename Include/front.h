e8302c22b56584ac10d092d61677a8501ef47886
apopova7-netizen <a.popova7@g.nsu.ru>
2026-05-01 13:38:43 +0700

    All common functions have been moved to the common.h file and missing functions have been added.

--- Include/front.h
+++ Include/front.h
@@ -1,22 +1,13 @@
 #ifndef FRONT_H
 #define FRONT_H
-
-#include <stdio.h> 
-#include <string.h> 
-#include <stdlib.h>
-
-typedef struct TreeNode{
-    char type;
-    char* data;             
-    struct TreeNode* right; 
-    struct TreeNode* left;  
-} TreeNode;
-
+include "common.h"
 
 char GetAnswer();
 char* GetNewWord();
 char* GetQuestion(char* wrongAnswer, char* rightAnswer);
 void Start();
+void DefaultTree();
+void DefaultTreeError();
 void PrintQuestion(TreeNode* cur);
 void Assumption(TreeNode* cur);
 void Victory();
